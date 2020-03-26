be4025ffd4b7037e2aa2161793103ec6a503cccf
gronnmann <gronnmannthecoder@gmail.com>
2020-03-26 13:33:39 +0100

    1.2

--- PasswordWhitelist/src/io/github/gronnmann/passwordwhitelist/PasswordWhitelist.java
+++ PasswordWhitelist/src/io/github/gronnmann/passwordwhitelist/PasswordWhitelist.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.util.List;
 
 import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
@@ -12,9 +13,14 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityTargetEvent;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerPickupItemEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import net.md_5.bungee.api.ChatColor;
@@ -72,26 +78,36 @@ public class PasswordWhitelist extends JavaPlugin implements Listener{
 	}
 	
 	private boolean approved(Player p) {
-		String player = p.getName();
-		if (approvedList.contains(player))return true;
-		else return false;
+		return approved(p, true);
 	}
 	
+	private boolean approved(Player p, boolean sendMsg) {
+		String player = p.getUniqueId().toString();
+		if (approvedList.contains(player))return true;
+		else{
+			if (sendMsg) {
+				p.sendMessage(ChatColor.RED + "Please specify password to interact with the world.");
+			}
+			return false;
+		}
+	}
 	
 	@EventHandler
 	public void joinManager(PlayerJoinEvent e) {
 		if (!approved(e.getPlayer())) {
-			e.getPlayer().sendMessage(ChatColor.RED + "Please specify password to get interaction access.");
+			e.getPlayer().setGameMode(GameMode.ADVENTURE);
 		}
 	}
 	
 	@EventHandler
 	public void checkForPassword(AsyncPlayerChatEvent e) {
-		if (!approved(e.getPlayer()) && e.getMessage().equals(password)) {
-			approvedList.add(e.getPlayer().getName());
+		if (!approved(e.getPlayer(), false) && e.getMessage().equals(password)) {
+			approvedList.add(e.getPlayer().getUniqueId().toString());
 			e.getPlayer().sendMessage(ChatColor.GREEN + "Verified successfully. Welcome :)");
 			e.setCancelled(true);
-			
+			Bukkit.getScheduler().runTask(this, ()->{
+				e.getPlayer().setGameMode(Bukkit.getServer().getDefaultGameMode());
+			});
 			saveApproved();
 		}
 	}
@@ -99,10 +115,39 @@ public class PasswordWhitelist extends JavaPlugin implements Listener{
 	@EventHandler
 	public void stopInteract(PlayerInteractEvent e) {
 		if (!approved(e.getPlayer())) {
-			e.getPlayer().sendMessage(ChatColor.RED + "Please specify password to get interaction access.");
 			e.setCancelled(true);
 		}
 	}
 	
+	@EventHandler
+	public void stopDrop(PlayerDropItemEvent e) {
+		if (!approved(e.getPlayer()))e.setCancelled(true);
+	}
 	
+	@EventHandler
+	public void stopPickup(PlayerPickupItemEvent e) {
+		if (!approved(e.getPlayer()))e.setCancelled(true);
+	}
+	
+	@EventHandler
+	public void stopEntityTarget(EntityTargetEvent e) {
+		if (!(e.getTarget() instanceof Player))return;
+		Player p = (Player)e.getTarget();
+		
+		if (!approved(p))e.setCancelled(true);
+	}
+	
+	@EventHandler
+	public void stopAttack(EntityDamageByEntityEvent e) {
+		if (!(e.getDamager() instanceof Player))return;
+		Player p = (Player)e.getDamager();
+		
+		if (!approved(p))e.setCancelled(true);
+		
+	}
+	
+	@EventHandler
+	public void disallowCommands(PlayerCommandPreprocessEvent e) {
+		if (!approved(e.getPlayer()))e.setCancelled(true);
+	}
 }
